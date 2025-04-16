b9cabf4a9b02fc3e6432a3679774e7b8b47490ff
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2025-04-16 13:49:02 +0200

    multiset progress i guess yes

--- src/data/multiset.ts
+++ src/data/multiset.ts
@@ -1,28 +1,38 @@
+/// <reference path="../ccs/ccs.ts" />
 module MultiSetUtil {
 
     type Entry = { proc: CCS.Process, weight: number };
 
     export class MultiSet {
-        public entries: Entry[];
+        private map: Record<string, Entry>
 
         constructor(entries: Entry[]) {
-            this.entries = entries;
-
+            this.map = {};
+            entries.forEach(e => this.add(e));
         }
 
-        private flattenDist() {
-            let flattened: Entry[] = [];
-
-            this.entries.forEach(entry => {
-                if (entry instanceof MultiSet) {
-
-                }
-            })
+        public getEntries() {
+            return Object.values(this.map);
         }
+
+        public size() {
+            return this.getEntries().map(x => x.weight).reduce((acc, curr) => acc + curr, 0);
+        }
+
+        public add(entry: Entry) {
+            const current = this.map[entry.proc.id] ?? { proc: entry.proc, weight: 0 };
+            current.weight += entry.weight;
+            this.map[entry.proc.id] = current;
+        }
+    }
+
+    export const weightedUnion = (setA: MultiSet, weightA: number, setB: MultiSet, weightB: number) => {
+        const aSize = setA.size();
+        const bSize = setB.size();
+        const aEntries = setA.getEntries().map(x => ({ proc: x.proc, weight: x.weight * bSize * weightA }));
+        const bEntries = setB.getEntries().map(x => ({ proc: x.proc, weight: x.weight * aSize * weightB }));
+
+        return new MultiSet([...aEntries, ...bEntries]);
     }
 }
 
-
-let entry1 = { proc: subProcesses[0], weight: probability.num };
-let entry2 = { proc: subProcesses[1], weight: probability.den - probability.num };
-let dist = new MultiSetUtil.MultiSet([entry1, entry2]);
